a32bfcb5bbcd7dd546c79d9adf4df4d78aa18704
Abishek B <abishekbalacbe@gmail.com>
2026-08-02 22:12:43 +0530

    Reorganize project structure and add new study materials

---
+++ Spring Boot(Code IO)/Start/Helloworld/src/main/java/com/example/Helloworld/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.example.Helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    String sayHelloWorld(){
+        return "Helloworld";
+    }
+}
